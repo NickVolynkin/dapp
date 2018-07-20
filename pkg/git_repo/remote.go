e1dc9a585604f2bb684b918b976a637daddcd911
Timofey Kirillov <timofey.kirillov@flant.com>
2018-07-20 17:58:37 +0300

    ++

--- pkg/git_repo/remote.go
+++ pkg/git_repo/remote.go
@@ -2,10 +2,14 @@ package git_repo
 
 import (
 	"fmt"
+	"github.com/flant/dapp/pkg/lock"
+	"gopkg.in/satori/go.uuid.v1"
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing"
 	"gopkg.in/src-d/go-git.v4/plumbing/storer"
 	"os"
+	"path/filepath"
+	"time"
 )
 
 type Remote struct {
@@ -14,42 +18,83 @@ type Remote struct {
 	ClonePath string // TODO: move CacheVersion & path construction here
 }
 
-func (repo *Remote) clone() error {
+func (repo *Remote) withLock(f func() error) error {
+	lockName := fmt.Sprintf("remote_git_artifact.%s", repo.Name)
+	return lock.WithLock(lockName, lock.LockOptions{Timeout: 600 * time.Second}, f)
+}
+
+func (repo *Remote) isCloneExists() (bool, error) {
+	_, err := os.Stat(repo.ClonePath)
+	if err == nil {
+		return true, nil
+	}
+
+	if !os.IsNotExist(err) {
+		return false, fmt.Errorf("cannot clone git repo: %s", err)
+	}
+
+	return false, nil
+}
+
+func (repo *Remote) Clone() error {
 	var err error
 
-	_, err = os.Stat(repo.ClonePath)
-	if err == nil {
+	exists, err := repo.isCloneExists()
+	if err != nil {
+		return err
+	}
+	if exists {
 		return nil
 	}
-	if !os.IsNotExist(err) {
-		return fmt.Errorf("cannot clone git repo: %s", err)
-	}
 
-	fmt.Printf("Clone remote git repo `%s`\n", repo.Url)
+	return repo.withLock(func() error {
+		exists, err := repo.isCloneExists()
+		if err != nil {
+			return err
+		}
+		if exists {
+			return nil
+		}
 
-	_, err = git.PlainClone(repo.ClonePath, true, &git.CloneOptions{
-		URL:               repo.Url,
-		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+		fmt.Printf("Clone remote git repo `%s` ...\n", repo.Url)
+
+		path := filepath.Join("/tmp", fmt.Sprintf("dapp-git-repo-%s", uuid.NewV4().String()))
+
+		_, err = git.PlainClone(path, true, &git.CloneOptions{
+			URL:               repo.Url,
+			RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+		})
+		if err != nil {
+			return fmt.Errorf("cannot clone git repo: %s", err)
+		}
+
+		defer os.RemoveAll(path)
+
+		err = os.MkdirAll(filepath.Dir(repo.ClonePath), 0755)
+		if err != nil {
+			return fmt.Errorf("cannot clone git repo: %s", err)
+		}
+
+		err = os.Rename(path, repo.ClonePath)
+		if err != nil {
+			return fmt.Errorf("cannot clone git repo: %s", err)
+		}
+
+		fmt.Printf("Clone remote git repo `%s` DONE\n", repo.Url)
+
+		return nil
 	})
-	if err != nil {
-		return fmt.Errorf("cannot clone git repo: %s", err)
-	}
+}
 
+func (repo *Remote) Fetch() error {
 	return nil
 }
 
 func (repo *Remote) HeadCommit() (string, error) {
-	var err error
-
-	err = repo.clone()
-	if err != nil {
-		return "", err
-	}
-
 	commit, err := repo.getHeadCommitForRepo(repo.ClonePath)
 
 	if err == nil {
-		fmt.Printf("Using commit `%s` of repository `%s`\n", commit, repo.Url)
+		fmt.Printf("Using HEAD commit `%s` of repository `%s`\n", commit, repo.Url)
 	}
 
 	return commit, err
@@ -81,11 +126,6 @@ func (repo *Remote) findReference(rawRepo *git.Repository, reference string) (st
 func (repo *Remote) LatestBranchCommit(branch string) (string, error) {
 	var err error
 
-	err = repo.clone()
-	if err != nil {
-		return "", err
-	}
-
 	rawRepo, err := git.PlainOpen(repo.ClonePath)
 	if err != nil {
 		return "", fmt.Errorf("cannot open repo: %s", err)
@@ -106,11 +146,6 @@ func (repo *Remote) LatestBranchCommit(branch string) (string, error) {
 
 func (repo *Remote) LatestTagCommit(tag string) (string, error) {
 	var err error
-
-	err = repo.clone()
-	if err != nil {
-		return "", err
-	}
 
 	rawRepo, err := git.PlainOpen(repo.ClonePath)
 	if err != nil {
